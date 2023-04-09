497e52dd2f21895746c14048e5e8e5b7115efda8
miketran <miketran@amazon.com>
2023-04-09 16:23:56 -0700

    feat: Add post details

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -5,10 +5,16 @@ interface tPostProps {
 }
 const Post = (postProps: tPostProps) => {
     const { post} = postProps;
-    const dateString = (new Date(post.time)).toDateString();
+    const dateString = (new Date(post.time *1000)).toLocaleString();
     return (
         <div>
             <h1>{post.title} </h1>
+            <p>Post Score: {post.score}</p>
+            <p>Post By: {post.by}</p>
+            <p>Post URL: {post.url}</p>
+            <p>Post Type: {post.type}</p>
+            <p>Post ID: {post.id}</p>
+            <p>Post Time: {post.time}</p>
             <p> {dateString} </p>
         </div>
     );
