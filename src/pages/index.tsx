1206f94ef9fc1dbe2a689e3e11f903594b835218
miketran <miketran@amazon.com>
2023-05-22 13:56:36 -0700

    feat: styling main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,18 +18,21 @@ export default function Home(props: tHomeProps) {
                 <br/>
             </div>
 
-            <p className="text-sm text-gray-500">This page is static site generated page (SSG). It was built on {formattedDate}.</p>
-            <Link href="/ssr">View a server-side rendered page.</Link>
+            <p className="text-sm text-gray-300">This page is static site generated page (SSG). It was built on {formattedDate}.</p>
+            <div className={"pt-6 pb-6"}>
+                <Link href="/ssr" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                      aria-current="page">View a server-side rendered page (SSR).</Link>
+            </div>
 
-            <p className="text-sm text-gray-500">This page demonstrates a SSG page where we pulled the data (list, detail) at the time of the build. Please click on each of the links.</p>
+            <p className="text-sm text-gray-300">This page demonstrates a SSG page where we pulled the data (list, detail) at the time of the build. Please click on each of the links.</p>
             <div className={"pt-6"}>
-                <h3  className={"text-xl font-bold tracking-tight text-white-900"}>List of Hacker News Post IDs</h3>
-                <ul className="divide-y divide-gray-200">
+                <h3  className={"text-xl font-bold tracking-tight text-white-900"}>List of Hacker News Post IDs from {formattedDate.slice(0,13)}</h3>
+                <ul className="divide-y divide-gray-900">
                     {props.posts.length > 0 && props.posts.map((postId: number) => (
                         <li key={postId} className="py-4 flex">
                             <div className="ml-3">
                                 <Link href={`/${postId}`}>
-                                    <p className="text-sm font-medium text-white-900">{postId}</p>
+                                    <p className="text-sm font-medium text-white-900">https://news.ycombinator.com/item?id=/{postId}</p>
                                 </Link>
                             </div>
                         </li>
--- src/pages/ssr.tsx
+++ src/pages/ssr.tsx
@@ -9,7 +9,7 @@ export default function SSR({ formattedDate }: { formattedDate: string }) {
             </p>
             <div className={"pt-6"}>
                 <Link href="/" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                      aria-current="page">View this SSR static page.</Link>
+                      aria-current="page">View the SSG pages.</Link>
             </div>
 
         </div>
